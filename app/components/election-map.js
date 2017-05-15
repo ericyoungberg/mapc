b3c348e262c1463932c93df2ec95051293c9c9e2
Eric Youngberg <eric@lmtlss.net>
2017-05-15 10:49:14 -0500

    Setup D3 boilerplate for Albers USA projection

--- app/components/election-map.js
+++ app/components/election-map.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  width: 960,
+  height: 500,
+
+  didRender() {
+    const results = this.get('results');
+
+    const projection = d3.geo.albersUsa()
+      .scale(1000)
+      .translate([this.get('width')/2, this.get('height')/2]);
+
+    const path = d3.geo.path().projection(projection);
+  }
+  
+
 });
