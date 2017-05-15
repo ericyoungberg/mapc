e6fe10efa14c58a531aedaf54ebf7506915b2dd7
Eric Youngberg <eric@lmtlss.net>
2017-05-15 12:58:14 -0500

    Fix multiple maps rendering when changing routes
    Every time that we load in the data for a different year, the map
    won't rerender with the new data. Instead, it will just append an entirely
    new svg map with the new data. This commit solves it by removing the
    existing svg element and then rendering a new one.

--- app/components/election-map.js
+++ app/components/election-map.js
@@ -5,7 +5,14 @@ export default Ember.Component.extend({
   width: 800,
   height: 500,
 
+  usPaths: null,
+
   didRender() {
+    this.renderMap();
+  },
+
+
+  renderMap() {
     const results = this.get('results');
 
     const projection = d3.geo.albersUsa()
@@ -14,11 +21,32 @@ export default Ember.Component.extend({
 
     const path = d3.geo.path().projection(projection);
 
+    /**
+     * We must remove the svg element if there is one. Otherwise we
+     * will not be able to render the map with the new dataset.
+     */
+    const svg = document.querySelector('.election-map > svg');
+    if (svg) {
+      svg.parentNode.removeChild(svg);
+    }
+
+    d3.select('.election-map')      
+      .append('svg')
+      .attr('class', 'states')
+      .attr('height', this.get('height'))
+      .attr('width', this.get('width'));
+
+
+    /**
+     * Collect the winners of each state so we will know how to
+     * color our map later.
+     */
     let winners = {};
     if (Object.values(results).length === 0) {
       this.set('noData', true);
     }
     else {
+      this.set('noData', false);
       // Find the winners for each state
       let winner, state, competitors;
       winners = Object.values(results).reduce((winners, result) => {
@@ -34,32 +62,45 @@ export default Ember.Component.extend({
       });
     }
 
-    // Following section built by referencing http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
-    d3.json('/assets/data/us-paths.json', (err, us) => {
-      if (err) throw err;
 
-      const svg = d3.select('.election-map')      
-        .append('svg')
-        .attr('class', 'states')
-        .attr('height', this.get('height'))
-        .attr('width', this.get('width'));
+    /**
+     * If we don't already have the GeoJSON USA states data loaded,
+     * load it in and then render our data.
+     *
+     * Following code built by referencing http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
+     */
+    if (this.get('usPaths') !== null) {
+      this.renderData(this.get('usPaths'), path, winners);
+    }
+    else {
+      d3.json('/assets/data/us-paths.json', (err, us) => {
+        if (err) throw err;
 
-      svg.selectAll('path') 
-        .data(us.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .attr('class', stateData => {
-          let state = stateData.properties.NAME;
+        this.set('usPaths', us);
+        this.renderData(us, path, winners);
+      });
+    }
+        
+  },
 
-          return (winners[state]) ? winners[state].parties[0].toLowerCase().replace(' ', '-') : '';
-        })
-        .on('click', stateData => {
-          let state = stateData.properties.NAME.toLowerCase().replace(' ', '-');
 
-          this.sendAction('showStateData', state);
-        });
-    });
+  renderData(us, path, winners) {
+     d3.select('svg')
+      .selectAll('path') 
+      .data(us.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .attr('class', stateData => {
+        let state = stateData.properties.NAME;
+
+        return (winners[state]) ? winners[state].parties[0].toLowerCase().replace(' ', '-') : '';
+      })
+      .on('click', stateData => {
+        let state = stateData.properties.NAME.toLowerCase().replace(' ', '-');
+
+        this.sendAction('showStateData', state);
+      });
   },
   
 
