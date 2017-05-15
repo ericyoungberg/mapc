24f21b655499e5602a0d3cd57e2b36b3800b53d2
Eric Youngberg <eric@lmtlss.net>
2017-05-15 10:07:16 -0500

    Fetch data from server from the year passed in the router
    Added measures to reroute the user to "/results/current" if the year that
    was found in the router is not a valid year for our dataset.

--- app/routes/results/by-year.js
+++ app/routes/results/by-year.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  beforeModel(transition) {
+    const yearFromRouter = String(transition.params['results.by-year'].year);
+    const validYears = ['2004', '2008', '2012', 'current'];
+    
+    if (!validYears.includes(yearFromRouter)) {
+      this.transitionTo('/results/current');
+    }
+  },
+
+  model(params) {
+    return (params.year === 'current') ? [] : Ember.$.ajax({url: `/assets/data/results_${params.year}.json`});
+  }
+
 });
