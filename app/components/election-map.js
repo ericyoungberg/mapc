b042fb7966d8d739187040a357e08b63a7d17970
Eric Youngberg <eric@lmtlss.net>
2017-05-15 10:58:26 -0500

    Write reducers to find the winner of each state

--- app/components/election-map.js
+++ app/components/election-map.js
@@ -8,11 +8,28 @@ export default Ember.Component.extend({
   didRender() {
     const results = this.get('results');
 
+    console.log(results);
+
     const projection = d3.geo.albersUsa()
       .scale(1000)
       .translate([this.get('width')/2, this.get('height')/2]);
 
     const path = d3.geo.path().projection(projection);
+
+    // Find the winners for each state
+    let winner, state, competitors;
+    const winners = Object.values(results).reduce((winners, result) => {
+      competitors = Object.values(result);
+
+      state = competitors[0].state;
+      winners[state] = Object.values(result).reduce((winning, competitor) => {
+        return (winning.votes > competitor.votes) ? winning : competitor;
+      });
+    
+      return winners;
+    });
+
+    console.log(winners);
   }
   
 
